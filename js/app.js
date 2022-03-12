34569662fa7af5db190936318270a9f5acdd1439
diptowebhero <diptodas1672@gmail.com>
2022-03-12 17:08:55 +0600

    done

--- js/app.js
+++ js/app.js
@@ -2,27 +2,93 @@ const previousOperandElement = document.querySelector(".previous_operant");
 const currentOperandElement = document.querySelector(".current_operant");
 const allClear = document.querySelector("#all-clear");
 const deleted = document.querySelector("#delete");
-const allOperator = document.querySelectorAll(".operator");
-const allNumber = document.querySelectorAll(".number");
+const allOperatorBtn = document.querySelectorAll(".operator");
+const allNumberBtn = document.querySelectorAll(".number");
 const equals = document.querySelector("#equals");
 
 let previousOperand = "";
 let currentOperand = "";
+let operation = "";
+
+function showNumberInDisplay() {
+  currentOperandElement.innerHTML = currentOperand;
+  previousOperandElement.innerHTML = `${previousOperand} ${operation}`;
+}
 
 function appendNumber(number) {
   currentOperand += number;
 }
 
-function updateNumber() {
-  currentOperandElement.innerHTML = currentOperand;
+function choseOPerator(operator) {
+  if (previousOperand) {
+    previousOperand = calculate();
+  } else {
+    previousOperand = currentOperand;
+  }
+  operation = operator;
+  currentOperand = "";
 }
 
-allNumber.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if ((btn.textContent === ".") & currentOperand.includes(".")) {
+//all clear btn
+allClear.addEventListener("click", function () {
+  previousOperand = "";
+  currentOperand = "";
+  operation = "";
+  showNumberInDisplay();
+});
+
+//calculate
+
+function calculate() {
+  let previous = Number(previousOperand);
+  let current = Number(currentOperand);
+  switch (operation) {
+    case "÷":
+      return previous / current;
+      break;
+    case "*":
+      return previous * current;
+      break;
+    case "+":
+      return previous + current;
+      break;
+    case "-":
+      return previous - current;
+      break;
+    case "%":
+      return previous % current;
+      break;
+  }
+}
+
+equals.addEventListener("click", function (e) {
+  if (!previousOperand) return;
+  if (currentOperand) {
+    currentOperand = calculate().toFixed(2);
+  } else {
+    currentOperand = previousOperand;
+  }
+  previousOperand = "";
+  operation = "";
+  showNumberInDisplay();
+});
+
+//display number
+allNumberBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    if ((this.textContent === ".") & currentOperand.includes(".")) {
       return;
     }
-    appendNumber(btn.textContent);
-    updateNumber();
+    appendNumber(this.textContent);
+    showNumberInDisplay();
+  });
+});
+
+//display operator
+allOperatorBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    if (!currentOperand) return;
+    choseOPerator(this.textContent);
+    showNumberInDisplay();
   });
 });
